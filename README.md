1e1cc86ef64392581a39aa9fb670d78a58af0874
Chris Maguire <cwmaguire@protonmail.com>
2025-03-21 22:10:02 -0700

    Support more color options
    Support RGB colors (38:2) and color by index (38:5, 48:5)
    
    (Hmm, I think I forgot to do background by RGB)
    
    Add another example of the 255 colors by index.

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Rendering terminal user interfaces from Erlang
 1. A grid of X & O squares
 
 ![xo squares](images/xo_squares.png "Contrasting X & O Squares")
+
+1. Colors by index
+
+![color by index](images/color_by_index.png "Colors by Index")
--- src/tui.erl
+++ src/tui.erl
@@ -1,10 +1,13 @@
 -module(tui).
 
+-export([escape/1]).
 -export([format/2]).
 -export([print/2]).
 
+% * = doesn't work in gnome-terminal
 -define(FEATURES,
         #{none => "0",
+          none_16 => "100",
           bold => "1",
           faint => "2",
           italic => "3",
@@ -23,6 +26,12 @@
           fg_magenta => "35",
           fg_cyan => "36",
           fg_white => "37",
+          fg_dark_red => "91",
+          fg_grey => "92",
+          fg_dark_yellow => "93", % *
+          fg_dark_blue => "94", %   *
+          fg_dark_magenta => "95",
+          fg_dark_cyan => "96", %   *
           bg_black => "40",
           bg_red => "41",
           bg_green => "42",
@@ -30,12 +39,33 @@
           bg_blue => "44",
           bg_magenta => "45",
           bg_cyan => "46",
-          bg_white => "47"}).
+          bg_white => "47",
+          bg_black_16 => "101",
+          bg_orange => "101",
+          bg_dark_green => "102", % grey
+          bg_dark_yellow => "103", % grey
+          bg_dark_blue => "104", % light grey
+          bg_dark_magenta => "105", % purple
+          bg_dark_cyan => "106", % grey
+          bg_white_16 => "107"
+         }).
 
 -define(ESC, 27).
 -define(CTRL_SEQ_INTRO, $[).
 -define(CHAR_ATTRIBUTE, $m).
+-define(RGB, "38:2:").
+-define(INDEXED_FG_COLOR, "38:5:").
+-define(INDEXED_BG_COLOR, "48:5:").
 
+escape({fg, IndexedColor}) when is_list(IndexedColor) ->
+    % "0" to "255"
+    [?ESC, ?CTRL_SEQ_INTRO, ?INDEXED_FG_COLOR, IndexedColor, ?CHAR_ATTRIBUTE];
+escape({bg, IndexedColor}) when is_list(IndexedColor) ->
+    % "0" to "255"
+    [?ESC, ?CTRL_SEQ_INTRO, ?INDEXED_BG_COLOR, IndexedColor, ?CHAR_ATTRIBUTE];
+escape({R, G, B}) ->
+    % "0" to "255" or R, G, and B
+    [?ESC, ?CTRL_SEQ_INTRO, ?RGB, R, $:, G, $:, B, ?CHAR_ATTRIBUTE];
 escape(Feature) ->
     #{Feature := Code} = ?FEATURES,
     [?ESC, ?CTRL_SEQ_INTRO, Code, ?CHAR_ATTRIBUTE].
@@ -43,6 +73,7 @@ escape(Feature) ->
 format(Text, Features) ->
     Escapes = [escape(F) || F <- Features],
     Normal = escape(none),
+    %Normal16 = escape(none_16),
     lists:flatten([Escapes, Text, Normal]).
 
 print(Text, Features) ->
