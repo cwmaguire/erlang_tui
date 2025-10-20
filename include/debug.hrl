7e12cb73fb22e553ec4271a037aee267846e3e61
Chris Maguire <cwmaguire@protonmail.com>
2025-10-19 20:53:55 -0700

    Track all debug lines in a single place
    I keep having collisions. I can also track the min and max debug lines
    to clear.

---
+++ include/debug.hrl
@@ -0,0 +1,14 @@
+-define(MIN_DEBUG_LINE, 10).
+-define(MAX_DEBUG_LINE, 25).
+-define(DEBUG_CMD, 10).
+-define(DEBUG_CMD_TO_INS, 10).
+-define(DEBUG_INS_TO_CMD, 12).
+-define(DEBUG_INS_TO_INS, 13).
+-define(DEBUG_CMD_INPUT, 14).
+-define(DEBUG_INS_INPUT, 15).
+-define(DEBUG_DISPATCH_INIT, 16).
+-define(DEBUG_NO_MAP, 17).
+-define(DEBUG_UNKNOWN_REQ, 18).
+-define(DEBUG_WINDOWS, 19).
+%% don't use 20; cs_command uses it.
+
